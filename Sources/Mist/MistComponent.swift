2093a660cab1cf1fd948bac90729e00fb2081028
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-05 02:48:02 +0100

    allman

--- Sources/Mist/MistComponent.swift
+++ Sources/Mist/MistComponent.swift
@@ -2,68 +2,65 @@ import Vapor
 import Fluent
 @testable import LeafKit
 
-public protocol Component: Sendable {
-    
+public protocol Component: Sendable
+{
     var name: String { get }
     var template: String { get }
     var models: [any Mist.Model.Type] { get }
     
     func render(id: UUID, on db: Database, using renderer: ViewRenderer) async -> String?
     func shouldUpdate<M: Mist.Model>(for model: M) -> Bool
-    
 }
 
-public extension Mist.Component {
-    
+public extension Mist.Component
+{
     var name: String { String(describing: Self.self) }
     var template: String { String(describing: Self.self) }
-    
 }
 
-public extension Mist.Component {
-    
-    func render(id: UUID, on db: Database, using renderer: ViewRenderer) async -> String? {
-        
+public extension Mist.Component
+{
+    func render(id: UUID, on db: Database, using renderer: ViewRenderer) async -> String?
+    {
         guard let context = await makeContext(of: id, in: db) else { return nil }
         guard let buffer = try? await renderer.render(template, context).data else { return nil }
-        
         return String(buffer: buffer)
     }
     
-    func shouldUpdate<M: Mist.Model>(for model: M) -> Bool {
+    func shouldUpdate<M: Mist.Model>(for model: M) -> Bool
+    {
         return models.contains { $0 == M.self }
     }
-    
 }
 
-public extension Mist.Component {
-    
-    func makeContext(of componentID: UUID, in db: Database) async -> SingleComponentContext? {
-        
+public extension Mist.Component
+{
+    func makeContext(of componentID: UUID, in db: Database) async -> SingleComponentContext?
+    {
         var container = Mist.ModelContainer()
-
-        for model in models {
-            
+        
+        for model in models
+        {
             guard let modelData = await model.find(componentID, db) else { continue }
             let modelName = String(describing: model).lowercased()
             
             container.add(modelData, for: modelName)
         }
-
+        
         guard container.isEmpty == false else { return nil }
         
         return SingleComponentContext(component: container)
     }
     
-    func makeContext(ofAll db: Database) async -> MultipleComponentContext {
-        
+    func makeContext(ofAll db: Database) async -> MultipleComponentContext
+    {
         var modelContainers: [Mist.ModelContainer] = []
 
         guard let primaryModelType = models.first else { return .empty }
         guard let primaryModels = await primaryModelType.findAll(db) else { return .empty }
 
-        for primaryModel in primaryModels {
-
+        for primaryModel in primaryModels
+        {
             guard let modelID = primaryModel.id else { continue }
             guard let modelContext = await makeContext(of: modelID, in: db) else { continue }
 
@@ -74,5 +71,4 @@ public extension Mist.Component {
 
         return Mist.MultipleComponentContext(components: modelContainers)
     }
-    
 }
