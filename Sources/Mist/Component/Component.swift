174e52dc957de3429cf976cddef7b0a541e2ce38
Berken Sayilir <sayilir.berken@gmail.com>
2026-05-03 18:01:13 +0200

    restore to working commit

--- Sources/Mist/Component/Component.swift
+++ Sources/Mist/Component/Component.swift
@@ -20,7 +20,7 @@ public protocol Component: Sendable {
 public extension Component {
     
     /// Default component name derived from the Swift type name.
-    var name: String { String(describing: Self.self) }
+    var name: String { String(describing: Self.self) } // asses using type(of:)
     
     /// Default template is a file with path matching `name`.
     var template: any Template { LeafTemplate.file(name) }
